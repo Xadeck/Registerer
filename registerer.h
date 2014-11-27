c7740b046e32acb819277c43899550302a347a5f
Xavier Décoret <x.decoret@me.com>
2014-11-27 01:15:30 +0100

    First version working with auto registration

--- registerer.h
+++ registerer.h
@@ -1,17 +1,47 @@
 #ifndef REGISTERER_H
 #define REGISTERER_H
 
+#include <memory>
 #include <mutex>
 #include <string>
 #include <map>
 #include <functional>
 
-class Registry {
+template <typename T>
+class Registerer {
   public:
-    using std::function<T*()> function_t;
-  private:
-    static std::mutex function_map_mutex_;
-    std::map<std::string, function_t> function_map_;
+    typedef std::function<T*()> function_t;
+    Registerer(const char* filename, int line, function_t function, const std::string& key) {
+      const Entry entry = { filename, line, function};
+      registry_mutex_.lock();
+      registry_.emplace(key, entry);
+      registry_mutex_.unlock();
+    }
+    
+    static std::unique_ptr<T> CreateByName(const std::string& key) {
+      std::unique_ptr<T> result;
+      registry_mutex_.lock();
+      auto entry_it = registry_.find(key);
+      if (entry_it != registry_.end()) {
+        result.reset(entry_it->second.function());
+      }
+      registry_mutex_.unlock();      
+      return std::move(result);
+    }
+  private:    
+    struct Entry {
+      const char* const filename;
+      const int line;
+      const function_t function;
+    };
+    static std::mutex registry_mutex_;
+    static std::map<std::string, Entry> registry_;
 };
 
-#undef REGISTERER_H
+template <typename T>
+std::mutex Registerer<T>::registry_mutex_;
+
+template <typename T>
+std::map<std::string, typename Registerer<T>::Entry> Registerer<T>::registry_;
+
+#endif // REGISTERER_H
--- registerer_test.cc
+++ registerer_test.cc
@@ -1,14 +1,45 @@
+#include "registerer.h"
 #include "gtest/gtest.h"
 
-#include <iostream>
-
-struct Point {
-  int x;
-  int y;
+class Vehicle {
+public:
+  virtual ~Vehicle() {}
+  
+  virtual int nb_wheels() const = 0;
 };
 
-TEST(Point, creation) {
-  const Point p = {1,2};
+// #define REGISTER(type) \
+//   Registerer<type>
+//
+// #define REGISTER_VEHICLE REGISTER(Vehicle)
+
+template <typename Trait, typename derived_type>
+struct AutoRegisterer {
+  static const Registerer<typename Trait::base_type> registerer;
+};
+
+template <typename Trait, typename derived_type>
+Registerer<typename Trait::base_type> const AutoRegisterer<Trait, derived_type>::registerer(__FILE__, __LINE__,
+  []() -> typename Trait::base_type* { return new derived_type; },
+  Trait::key());
+
+class Car : public Vehicle {
+public:
+  struct VehicleTrait {
+    typedef Vehicle base_type;
+    static const char* key() { return "Car"; }
+  };
+  const void* unused_Vehicle_pointer() {
+    return &AutoRegisterer<VehicleTrait, Car>::registerer;
+  }
   
-  EXPECT_EQ(2, p.y);
+  int nb_wheels() const override { return 4; }  
+};
+
+
+
+TEST(Point, creation) {
+  auto vehicle = Registerer<Vehicle>::CreateByName("Car");
+  ASSERT_TRUE(vehicle.get());
+  EXPECT_EQ(4, vehicle->nb_wheels());
 }
